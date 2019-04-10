76a20874a193c780b560cd9b2dd2c7b0f93a7f0a
MitevskaS <mitevska138@gmail.com>
2019-04-10 15:40:07 +0200

    Updates

---
+++ precache-manifest.40a5e321fee0f33d075ba5ec2071e517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed32cb9d1e89fe11ac8c",
+    "url": "/React-Redux-Task/static/css/main.9920bcf3.chunk.css"
+  },
+  {
+    "revision": "ed32cb9d1e89fe11ac8c",
+    "url": "/React-Redux-Task/static/js/main.d500e8d6.chunk.js"
+  },
+  {
+    "revision": "4a5eaec3bbc4300ee0d3",
+    "url": "/React-Redux-Task/static/js/runtime~main.65d62f39.js"
+  },
+  {
+    "revision": "a402069d3b51bc088a0a",
+    "url": "/React-Redux-Task/static/js/2.12310606.chunk.js"
+  },
+  {
+    "revision": "5a92c3000409534aa5a58da1a134ddbf",
+    "url": "/React-Redux-Task/index.html"
+  }
+];
